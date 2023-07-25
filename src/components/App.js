4a12f9dea5d37fcd63667d848f44bf4811895e7a
alexgoit <alexandrpuryaev@gmail.com>
2023-07-25 14:33:36 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
@@ -9,76 +9,53 @@ import Loader from "./Loader/Loader";
 
 import getWeather from "../services/weather-api";
 
-export default class App extends Component {
-  state = {
-    weather: {},
-    city: "",
-    update: false,
-    isLoading: false,
-  };
+const App = () => {
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("");
+  const [update, setUpdate] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
-  componentDidMount() {
-    this.setState({ city: "Kryvyy Rih" });
+  useEffect(() => {
+    setCity("Kryvyy Rih");
+  }, []);
 
-    // setInterval(() => {
-    //   this.setState((prevState) => {
-    //     return { update: !prevState.update };
-    //   });
-    // }, 60000);
-  }
+  useEffect(() => {
+    const fetchWeather = async () => {
+      setLoading(true);
 
-  componentDidUpdate(_, prevState) {
-    const { city, update } = this.state;
+      try {
+        const weather = await getWeather(city);
 
-    if (prevState.city !== city || prevState.update !== update) {
-      this.fetchWeather();
+        setWeather(() => ({ ...weather }));
+      } catch (err) {
+        console.log("fetchWeather", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (city !== "") {
+      fetchWeather();
     }
-  }
+  }, [city, update]);
 
-  componentDidCatch(err, info) {
-    console.log(err, info);
-  }
+  setInterval(() => {
+    setUpdate(!update);
+  }, 300000);
 
-  fetchWeather = async () => {
-    this.toggleLoader();
+  const { location, current, forecast } = weather;
+  const isNotEmptyWeather = location ? true : false;
 
-    try {
-      const weather = await getWeather(this.state.city);
+  return (
+    <Container maxWidth="sm" sx={{ padding: 2 }} className="App">
+      {isNotEmptyWeather && (
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <CurrentWeather current={current} location={location} />
+          <ForecastWeather forecast={forecast} />
+        </Box>
+      )}
+      {isLoading && <Loader />}
+    </Container>
+  );
+};
 
-      this.setState({ weather });
-    } catch (err) {
-      console.log("fetchWeather", err);
-    } finally {
-      this.toggleLoader();
-    }
-  };
-
-  toggleLoader = () => {
-    this.setState((prevState) => {
-      return { isLoading: !prevState.isLoading };
-    });
-  };
-
-  render() {
-    const { weather, isLoading } = this.state;
-
-    const isNotEmptyWeather = Object.keys(weather).length !== 0;
-
-    console.log("render", isNotEmptyWeather);
-
-    return (
-      <Container maxWidth="sm" sx={{ padding: 2 }} className="App">
-        {isNotEmptyWeather && (
-          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            <CurrentWeather
-              current={weather.current}
-              location={weather.location}
-            />
-            <ForecastWeather forecast={weather.forecast} />
-          </Box>
-        )}
-        {isLoading && <Loader />}
-      </Container>
-    );
-  }
-}
+export default App;
